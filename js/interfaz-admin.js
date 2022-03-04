48ac33fa7364843c725974b5bdd79c997c675a02
Juan Cruz Agüero Luna <89282280+gorguero@users.noreply.github.com>
2022-03-04 17:46:45 -0300

    Creacion archivo interfaz-admin con JavaScript
    Se ha agregado el archivo de javascript correspondiente a la pagina de administrador.

---
+++ js/interfaz-admin.js
@@ -0,0 +1,16 @@
+//VARIABLES
+const btnEditarPerfil = document.getElementById('editar-perfil');
+const seccionPerfil = document.getElementById('seccion-perfil');
+
+
+//EVENTOS
+btnEditarPerfil.addEventListener('click', editarPerfil);
+
+
+
+//FUNCIONES
+
+//Funcion para habilitar la edicion del perfil
+function editarPerfil(){
+    console.log(seccionPerfil);
+}
